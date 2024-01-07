a8e490948ff1be4b32c26cb3c624c2fa91d0286e
Nisha Naziya <119906732+Nishanaziya@users.noreply.github.com>
2024-01-07 09:24:44 +0530

    Add files via upload

---
+++ array_adapter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.array_adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String [] name ={"anas","vinu","nisha"};
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        l = (ListView) findViewById(R.id.l);
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,name);
+        l.setAdapter(ad);
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent in = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(in);
+            }
+        });
+    }
+}
